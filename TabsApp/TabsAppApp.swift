751385656b68cba1a8151a5c1f2cfcc5ae64dfd5
Rene <rene.soto01@iest.edu.mx>
2025-01-10 19:22:40 -0600

    Initial Commit

---
+++ TabsApp/TabsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabsAppApp.swift
+//  TabsApp
+//
+//  Created by rene on 10/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TabsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
