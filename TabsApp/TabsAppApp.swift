e19beb776af1152d49765c0d16c7c3ef0a381b69
Rene <rene.soto01@iest.edu.mx>
2025-04-14 22:25:10 -0600

    Notificaciones antes de cocoa

--- TabsApp/TabsAppApp.swift
+++ TabsApp/TabsAppApp.swift
@@ -6,13 +6,35 @@
 //
 
 import SwiftUI
+import OneSignalFramework
 
 @main
 struct TabsAppApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+       
+    
     var body: some Scene {
         WindowGroup {
             //SignUpView()
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      
+       // Enable verbose logging for debugging (remove in production)
+       OneSignal.Debug.setLogLevel(.LL_VERBOSE)
+       // Initialize with your OneSignal App ID
+       OneSignal.initialize("35c47ca5-7ae1-4a4f-a61b-8aa02e268d90", withLaunchOptions: launchOptions)
+       // Use this method to prompt for push notifications.
+       // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
+       OneSignal.Notifications.requestPermission({ accepted in
+         print("User accepted notifications: \(accepted)")
+       }, fallbackToSettings: false)
+      
+       return true
     }
 }
